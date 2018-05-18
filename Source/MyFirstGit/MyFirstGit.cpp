ca870353cd3040d206363321c1ca43983fbdaca7
tamaras1023 <36715044+tamaras1023@users.noreply.github.com>
2018-05-18 11:14:13 -0400

    Initial commit

---
+++ Source/MyFirstGit/MyFirstGit.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyFirstGit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFirstGit, "MyFirstGit" );
